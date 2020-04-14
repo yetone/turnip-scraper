ff62c48e89b46b5e5fbcea5ac750dc9f64818e58
VeryCB <imcaibin@gmail.com>
2020-04-14 16:52:03 +1200

    feat: open queue website directly for Mac

--- turnip-scraper.py
+++ turnip-scraper.py
@@ -1,12 +1,19 @@
 from __future__ import print_function
 import requests
+import os
 import threading
 import sys
 import argparse
 import time
 import json
-from playsound import playsound
 
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Safari/605.1.15'}
+COOKIES = {
+    '__cfduid': 'db1eb4357d63476a501dfd1f76aabb4801586213874',
+    '_ga_ccuid_v2': 'QZ4CfTwZ5%2B5MIB4UTteY1Q%3D%3D%3A%3Atqrs%2FxGGlTJqOpWSIpLP779obLM2vPSA%2BLvLa0eZwuAK%2BD7aNVO9Aq2lTDojX8daeV%2FBY%2BXYKJPbb03dwo7h%2BPpx%2Bn79l2UlBLki4JLCwVudGcwtnMBqf1n7AlqlVITbvR7b90s43WDj%2F8%2F5o95cTOOPGyiY7NmB%2Feqd41OMT%2FDgMbLq3ElXgct26V47Wup7VkbXR25colMIpB%2Bmh%2FjBJs2%2BFvU1bmeB07Uv4AcBpv%2BVfzIwDPpd6jf48TcF4GvpKKgeZMiUzyGezMYuQKsX2TjIpASDgOyQqvssxyJyc%2BP3anaYIsB6cV0iCvjFxy%2BChN3Gbfftn0hvfKb7dAZjRdP54Thex3SPsBdYitrEGG6f4drVUqWFRo8RryI2q7hM8F2LG3BCptmcdMd5y6nUqkJZteIvYTdo0rLkmdJGkwhKwTlmfn%2BDxf1fz9wf07DD9MZbN2bc4crHJvzoCuFOM5t2K4Ic1ZtPcmrQqGc5kVgOogBMKp9u5m7VrPWoh5sJHqaI7b8h0K2LZGf7mGXws0qZ%2BrFV8m0WQWC0sluyvefwdLm9faZ6JLyhtCWy05Cq6QegJ%2FbLokH84pyhSfU1sXwv78s1BeOhMaOzO0CUg5PhawFfvegd4fS%2BGx%2FEKtY%2FUbFTC69DAa4og3%2Fk13h4uRr95mNhGPTGVPEhqP0BsWljKvZjGlWQ9w3HC2K9Mu8v',
+    'adbe_omni_usr': '840c1450-a7b4-5285-90bf-a0c5afec6b91',
+}
 
 screenlock = threading.Semaphore(value=1)
 
@@ -61,7 +68,7 @@ class TurnipScraper:
         print("Description:\t\t%s" % turnipcodes[turnipcode]["description"])
         print("Copy and paste the following link to your browser and join the queue:")
         print("\nhttps://turnip.exchange/island/%s\n\n" % turnipcode)
-        playsound('SFX.mp3')
+        os.system("open https://turnip.exchange/island/%s" % turnipcode)
 
     def CriteriaCheck(self, turnipcode):
         if turnipcodes[turnipcode]["turnipPrice"] >= int(self.price) and turnipcodes[turnipcode]["queued"] <= int(self.queue):
@@ -72,8 +79,9 @@ class TurnipScraper:
     def NookCrook(self):
         try:
             result = None
-            response = requests.get(self.API_URL + "/islands")
+            response = requests.get(self.API_URL + "/islands", headers=HEADERS, cookies=COOKIES)
             try:
+                print(response)
                 result = response.json()
             except json.decoder.JSONDecodeError:
                 print("[X] Island API call returned nothing, you may need to set your time interval higher")
@@ -132,8 +140,8 @@ class Main:
       @@@@@@@@@@@@@@@@@@@@@@@@@    
       @@@@@@@@@@@@@@@@@@@@@@@@@@@  Turnip Exchange Scraper Service
      @@@@@@@@@@@@@@@@@@@@@@@@@@@@  STALNKS!
-    @@@@@@@@@@@@@@@@    &@@@@@@@@  @bashexplode
-   @@@@@@@@@@@@@@(         @@@@@   
+    @@@@@@@@@@@@@@@@    &@@@@@@@@  Original Author @bashexplode
+   @@@@@@@@@@@@@@(         @@@@@   Modified by @VeryCB
    @@@@@@@@@@@@@@          @@@     
        @@@@@@@@@@@                                        
                    """)
